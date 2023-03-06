179a85a7ddee95020fa0258eeec29fe90d194a41
De <spark49942@gmail.com>
2023-03-06 06:14:09 +0300

    PrZ-0

---
+++ Toyfab.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Toyfab 
+{
+
+    public Map<Integer, String> toys;
+    public int[] vals = {75, 25, 50, 42, 65, 90, 10};
+    public void Toyfab(String[] abs, int key)
+    {
+        toys.put(1, "Игрушка-1");
+        toys.put(2, "Игрушка-2");
+        toys.put(3, "Игрушка-3");
+        toys.put(4, "Игрушка-4");
+        toys.put(5, "Игрушка-5");
+        toys.put(6, "Игрушка-6");
+        toys.put(7, "Игрушка-7");
+        System.out.println(vals[0]);
+    }
+    
+}
