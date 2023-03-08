4f0268329e771b6a024bcb015bb3aaffe3342d6a
De <spark49942@gmail.com>
2023-03-08 05:37:10 +0300

    2-btvrsn

--- Toyfab.java
+++ Toyfab.java
@@ -2,29 +2,38 @@ import java.util.*;
 
 public class Toyfab 
 {
-    int count;
+    int count = 1;
     public int[] vals = {75, 25, 50, 42, 65, 90, 10, 50};
     public Map<Integer, String> toys;
-    int key = 1;
+    int key;
     public Toyfab(int nmbr) 
     {
         this.toys = new HashMap<Integer, String>();
         Random rnd = new Random();
         int vlsize = vals.length;
-        toys.put(1, "Игрушка-1");
-        toys.put(2, "Игрушка-2");
-        toys.put(3, "Игрушка-3");
-        toys.put(4, "Игрушка-4");
-        toys.put(5, "Игрушка-5");
-        toys.put(6, "Игрушка-6");
-        toys.put(7, "Игрушка-7");
+        toys.put(1, "Кукла Барби");
+        toys.put(2, "Боевой робот");
+        toys.put(3, "Плюшевый мишка");
+        toys.put(4, "Конструктор LEGO");
+        toys.put(5, "Вкусная конфета");
+        toys.put(6, "Йо-Йо");
+        toys.put(7, "Машинка");
         for (int i = 0; i<vlsize; i++) 
         {
+            if (vals[i]>vals[nmbr]) 
+            {
+                int rng = rnd.nextInt(2 - 0) + 0;
+                if (rng != 0)
+                {
+                    this.key = i;
+                }
+            }
             this.key = rnd.nextInt(vlsize - 0) + 0;
         }
         if (nmbr == key) 
         {
-            System.out.println("Поздравляю! Ты получил " + toys.get(nmbr) + "!"); 
+            System.out.println("Поздравляю! Ты получил " + toys.get(nmbr) + "!");
+
         }
         else if (key == 0) 
         {
